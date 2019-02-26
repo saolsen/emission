2477e34e1f826c4f4db924a9723c7493dc98c3a2
Orta Therox <orta.therox@gmail.com>
2019-02-26 14:08:27 -0500

    Fix timezone tests

--- src/lib/Scenes/Fair/Components/FairHeader/__tests__/CountdownTimer-tests.tsx
+++ src/lib/Scenes/Fair/Components/FairHeader/__tests__/CountdownTimer-tests.tsx
@@ -1,4 +1,5 @@
 import { render } from "enzyme"
+import { mockTimezone } from "lib/tests/mockTimezone"
 import React from "react"
 import { CountdownTimer } from "../CountdownTimer"
 
@@ -6,20 +7,19 @@ const dateString = m => new Date(m).toISOString()
 
 describe("CountdownTimer", () => {
   beforeEach(() => {
+    mockTimezone("America/Los_Angeles")
     jest.useFakeTimers()
     Date.now = () => 1525983752000 // Thursday, May 10, 2018 8:22:32.000 PM UTC in milliseconds
   })
 
   it("renders upcoming properly", () => {
-    const comp = render(
-      <CountdownTimer startAt={dateString(Date.now() + 1000)} endAt={dateString(Date.now() + 2000)} />
-    )
-    expect(comp.text()).toContain("Opens May 10 at 8pm")
+    const comp = render(<CountdownTimer startAt="2018-05-10T20:22:42+00:00" endAt="2018-05-14T10:24:31+00:00" />)
+    expect(comp.text()).toContain("Opens May 10 at 4pm")
   })
 
   it("renders current properly", () => {
-    const comp = render(<CountdownTimer startAt={dateString(Date.now())} endAt={dateString(Date.now() + 1000)} />)
-    expect(comp.text()).toContain("Closes May 10 at 8pm")
+    const comp = render(<CountdownTimer startAt="2018-04-14T20:00:00+00:00" endAt="2018-05-14T20:00:00+00:00" />)
+    expect(comp.text()).toContain("Closes May 14 at 4pm")
   })
 
   it("renders closed properly", () => {
